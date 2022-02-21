032d789d0dedc98a88cebef282c8ad907ba1069d
juustkim <kimmo@juustovaara.fi>
2022-02-21 21:04:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ fi
 sed -i -e "s/local.example.com/$domain/g" /docker/docker-compose.yml
 sed -i -e "s/user@example.com/$email/g" /docker/docker-compose.yml
 sed -i -e "s/YOU_API_TOKEN/$cftoken/g" /docker/docker-compose.yml
-sed -i -e "s/USER:BASIC_AUTH_PASSWORD/$basicauth/g" /docker/docker-compose.yml
+sed -i -e "s/BASICAUTH/$basicauth/g" /docker/docker-compose.yml
 sed -i -e "s/user@example.com/$email/g" /docker/traefik/data/traefik.yml
 touch /docker/traefik/data/acme.json
 chmod 600 /docker/traefik/data/acme.json
