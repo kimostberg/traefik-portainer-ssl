98538f50c96e8f0344437aeeeccb5cc1f46880bc
juustkim <kimmo@juustovaara.fi>
2022-02-21 21:37:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOCKERDIR=/docker/traefik/data
+DOCKERDIR=/docker/container/traefik/data
 
 if [ ! -d $DOCKERDIR ]; then
     mkdir -p $DOCKERDIR
@@ -10,25 +10,24 @@ read -p 'Username: ' uservar
 read -p 'Password: ' passvar
 read -p 'Email: ' email
 read -p 'Domain: ' domain
-read -p 'CloudFlare DNS API TOKEN: ' cftoken 
+read -p 'CloudFlare DNS API TOKEN: ' cftoken
 sudo apt update
 sudo apt install apache2-utils git -y
-hash=$(htpasswd -nb $uservar $passvar | grep -oP ":\K.*")
+basicauth=`htpasswd -nb $uservar $passvar`
 
 if [ ! -f /docker/docker-compose.yml ]; then
     sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/docker-compose.yml -P /docker
 fi
 
 if [ ! -f /docker/traefik/data/traefik.yml ]; then
-    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/traefik/data/traefik.yml -P /docker/traefik/data
+    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/traefik/data/traefik.yml -P $DOCKERDIR
 fi
 
 sed -i -e "s/local.example.com/$domain/g" /docker/docker-compose.yml
 sed -i -e "s/user@example.com/$email/g" /docker/docker-compose.yml
 sed -i -e "s/YOU_API_TOKEN/$cftoken/g" /docker/docker-compose.yml
-sed -i -e "s/USER/$uservar/g" /docker/docker-compose.yml
-sed -i -e "s/BASICAUTH/$hash/g" /docker/docker-compose.yml
-sed -i -e "s/user@example.com/$email/g" /docker/traefik/data/traefik.yml
-touch /docker/traefik/data/acme.json
-chmod 600 /docker/traefik/data/acme.json
-touch /docker/traefik/data/config.yml
+sed -i -e "s/BASICAUTH/$basicauth/g" /docker/docker-compose.yml
+sed -i -e "s/user@example.com/$email/g" $DOCKERDIR/traefik.yml
+touch $DOCKERDIR/acme.json
+chmod 600 $DOCKERDIR/acme.json
+touch $DOCKERDIR/config.yml
