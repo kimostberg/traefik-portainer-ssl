af322526d51744780dcd9405a475cd2ba78c0028
juustkim <kimmo@juustovaara.fi>
2022-02-22 13:02:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ read -p 'Username: ' uservar
 read -p 'Password: ' passvar
 read -p 'Email: ' email
 read -p 'Domain: ' domain
-read -p 'CloudFlare DNS API TOKEN: ' cftoken
+read -p 'CloudFlare API KEY: ' cftoken
 sudo apt update
 sudo apt install apache2-utils git -y
-basicauth=$(htpasswd -nb $uservar $passvar` | grep -oP ":\K.*")
+basicauth=$(echo $(htpasswd -nb $uservar $passvar) | sed -e s/\\$/\\$\\$/g)
 
 if [ ! -f /docker/docker-compose.yml ]; then
     sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/docker-compose.yml -P /docker
@@ -25,8 +25,7 @@ fi
 
 sed -i -e "s/local.example.com/$domain/g" /docker/docker-compose.yml
 sed -i -e "s/user@example.com/$email/g" /docker/docker-compose.yml
-sed -i -e "s/YOU_API_TOKEN/$cftoken/g" /docker/docker-compose.yml
-sed -i -e "s/USER/$uservar/g" /docker/docker-compose.yml
+sed -i -e "s/YOU_API_KEY/$cftoken/g" /docker/docker-compose.yml
 sed -i -e "s/BASICAUTH/$basicauth/g" /docker/docker-compose.yml
 sed -i -e "s/user@example.com/$email/g" $DOCKERDIR/traefik.yml
 touch $DOCKERDIR/acme.json
