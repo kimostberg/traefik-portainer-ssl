e732e9b48fd9d84e42109b7f82d8f50d0c054894
juustkim <kimmo@juustovaara.fi>
2022-02-23 10:02:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-DOCKERDIR=/docker/container/traefik/data
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
+DOCKERDIR=/docker/container/
 
 if [ ! -d $DOCKERDIR ]; then
     mkdir -p $DOCKERDIR
@@ -12,22 +29,40 @@ read -p 'Email: ' email
 read -p 'Domain: ' domain
 read -p 'CloudFlare API KEY: ' cftoken
 sudo apt update
-sudo apt install apache2-utils git -y
+sudo apt install apache2-utils git wget docker-compose -y
 basicauth=$(echo $(htpasswd -nb $uservar $passvar) | sed -e s/\\$/\\$\\$/g)
 
-if [ ! -f /docker/docker-compose.yml ]; then
-    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/docker-compose.yml -P /docker
+mkdir -p $DOCKERDIR/traefik-portainer
+
+if [ ! -f $DOCKERDIR/traefik-portainer/docker-compose.yml ]; then
+    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/docker-compose.yml -P $DOCKERDIR/traefik-portainer
+fi
+mkdir -p $DOCKERDIR/traefik/data
+
+if [ ! -f $DOCKERDIR/traefik/data/traefik.yml ]; then
+    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/traefik/data/traefik.yml -P $DOCKERDIR/traefik/data
 fi
 
-if [ ! -f /docker/traefik/data/traefik.yml ]; then
-    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/traefik/data/traefik.yml -P $DOCKERDIR
+sed -i -e "s/local.example.com/$domain/g" $DOCKERDIR/traefik-portainer/docker-compose.yml
+sed -i -e "s/user@example.com/$email/g" $DOCKERDIR/traefik-portainer/docker-compose.yml
+sed -i -e "s/YOU_API_KEY/$cftoken/g" $DOCKERDIR/traefik-portainer/docker-compose.yml
+sed -i -e "s/BASICAUTH/$basicauth/g" $DOCKERDIR/traefik-portainer/docker-compose.yml
+sed -i -e "s/user@example.com/$email/g" $DOCKERDIR/traefik-portainer/traefik/traefik.yml
+touch $DOCKERDIR/traefik/data/acme.json
+chmod 600 $$DOCKERDIR/traefik/data/acme.json
+if [ ! -f $DOCKERDIR/traefik/data/config.yml ]; then
+    sudo wget https://raw.githubusercontent.com/kimostberg/traefik-portainer-ssl/main/traefik/data/config.yml -P $DOCKERDIR/traefik/data
 fi
 
-sed -i -e "s/local.example.com/$domain/g" /docker/docker-compose.yml
-sed -i -e "s/user@example.com/$email/g" /docker/docker-compose.yml
-sed -i -e "s/YOU_API_KEY/$cftoken/g" /docker/docker-compose.yml
-sed -i -e "s/BASICAUTH/$basicauth/g" /docker/docker-compose.yml
-sed -i -e "s/user@example.com/$email/g" $DOCKERDIR/traefik.yml
-touch $DOCKERDIR/acme.json
-chmod 600 $DOCKERDIR/acme.json
-touch $DOCKERDIR/config.yml
+sudo docker network create \
+  --driver=bridge \
+  --subnet=172.20.0.0/16 \
+  --gateway=172.20.0.1 \
+  frontend
+
+sudo docker network create \
+  --driver=bridge \
+  --subnet=172.21.0.0/16 \
+  backend
+
+docker-compose up -d -f $DOCKERDIR/traefik-portainer/docker-compose.yml
